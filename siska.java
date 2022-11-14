b815e7b25dbd3af1c5d9810a2e2af00536c9b5a1
Siskaaaaaa <118108619+Siskaaaaaa@users.noreply.github.com>
2022-11-14 18:54:49 +0700

    Create siska.java

---
+++ siska.java
@@ -0,0 +1,23 @@
+package uts;
+public class siskaUTS {
+    static String[] Kopi_Latte_Dont_Be_Late = new String[400];
+    static String[] kopi_gonceng_jiwa = new String[300];
+    static String[] kopi_jalan_kenangan = new String[300];
+    static String[] kopi_tanpa_rasa = new String[250];
+
+    static int stok = jangan_be_terlambat.panjang + kopi_gonceng_jiwa.panjang + kopi_jalan_kenangan.panjang + kopi_tanpa_rasa.panjang;
+    static int sisa = stok;
+
+    public static void main (String [] args ){
+        Pesan ();
+    }
+    static void print_data_pesanan (){
+        int total_kopidontbelste = 0 ;
+        for (int i = 0 ; i < kopi_dont_be_late . length ; i ++ ){
+            if (kopi_be_late [i] ! = null ){
+            }
+        }
+        int total_kopigoncengjiwa = 0;
+        for (int i = 0 ; i < )
+    }
+}
